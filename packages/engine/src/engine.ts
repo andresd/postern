d0129b4384d5a9d04edc54101969d47cb2db41f5
Andres Denkberg <andres.denkberg@gmail.com>
2022-07-07 16:08:49 +0300

    response updates

--- packages/engine/src/engine.ts
+++ packages/engine/src/engine.ts
@@ -3,7 +3,7 @@ import http from 'http'
 import cors from 'cors'
 import url from 'url'
 import { match } from 'node-match-path'
-import { chalky, methodColor, MockServer, terminal } from '@postern/core'
+import { chalky, HttpMethod, methodColor, MockServer, terminal } from '@postern/core'
 import { generateBody } from './templateManager'
 
 const app = express()
@@ -48,55 +48,33 @@ app.all(/.*/, (req, res) => {
     const forwardProxy = currentServer.forwardProxy
     const endpoints = currentServer.endpoints
 
-    endpoints
-      .filter(endpoint => {
-        if (endpoint.method !== req.method) {
-          return false
-        }
-
-        // const url = new URL(req.url)
+    const endpoint = currentServer.findEndpoint(req.method as HttpMethod, req.url)
+    if (endpoint) {
+      if (endpoint.redirectEnabled && endpoint.redirect) {
+        //
+        // Redirect
+        let redirectPath = endpoint.redirect
         // eslint-disable-next-line n/no-deprecated-api
         const pathname = url.parse(req.url).pathname
-        const endpointPath = `/api/${endpoint.path[0] === '/' ? endpoint.path.substring(1) : endpoint.path}`
+        const endpointPath = `/ api / ${endpoint.path[0] === '/' ? endpoint.path.substring(1) : endpoint.path} `
         const pathMatchResult = pathname ? match(endpointPath, pathname) : null
-        if (!pathMatchResult?.matches) {
-          return false
-        }
+        const params = pathMatchResult?.params
 
-        return true
-      })
-      .forEach((endpoint, index) => {
-        if (index > 0) {
-          return
-        }
+        params && Object.entries(params).forEach(param => {
+          redirectPath = redirectPath.replace(`:${param[0]} `, param[1])
+        })
+        let queryString = ''
+        req.query && Object.entries(req.query).forEach(param => {
+          queryString += `${queryString ? '&' : '?'}${param[0]}=${param?.[1]?.toString() ?? ''} `
+        })
 
-        terminal.info(colors[req.method](req.method), ' ', req.url)
+        terminal.info(colors.redirect('REDIRECTED TO: '), colors[req.method](req.method), ' ', req.url)
 
-        if (endpoint.redirectEnabled && endpoint.redirect) {
-          //
-          // Redirect
-          let redirectPath = endpoint.redirect
-          // eslint-disable-next-line n/no-deprecated-api
-          const pathname = url.parse(req.url).pathname
-          const endpointPath = `/ api / ${endpoint.path[0] === '/' ? endpoint.path.substring(1) : endpoint.path} `
-          const pathMatchResult = pathname ? match(endpointPath, pathname) : null
-          const params = pathMatchResult?.params
-
-          params && Object.entries(params).forEach(param => {
-            redirectPath = redirectPath.replace(`:${param[0]} `, param[1])
-          })
-          let queryString = ''
-          req.query && Object.entries(req.query).forEach(param => {
-            queryString += `${queryString ? '&' : '?'}${param[0]}=${param?.[1]?.toString() ?? ''} `
-          })
-
-          terminal.info(colors.redirect('REDIRECTED TO: '), colors[req.method](req.method), ' ', req.url)
-
-          res.redirect(307, redirectPath + queryString)
-        } else {
-          //
-          // Without Redirect
-          const response = endpoint.responses?.find(response => response.isActive)
+        res.redirect(307, redirectPath + queryString)
+      } else {
+        const response = currentServer.getValidResponse(endpoint, req.url, req.headers, req.body)
+        if (response) {
+          terminal.info(colors[req.method](req.method), ' ', req.url)
 
           if (response?.headers) {
             res.set(response.headers)
@@ -107,8 +85,9 @@ app.all(/.*/, (req, res) => {
 
           res.status(response?.statusCode ?? 200).send(getSafeJson(body))
         }
-        processed = true
-      })
+      }
+      processed = true
+    }
 
     if (!processed) {
       if (forwardProxy) {
@@ -117,7 +96,7 @@ app.all(/.*/, (req, res) => {
         res.redirect(307, redirectTo)
       } else {
         terminal.info(colors.notFound('ENDPOINT NOT FOUND - '), colors[req.method](req.method), ' ', req.url)
-        res.status(404).send('fail!')
+        res.status(404).send('fail')
       }
     }
   } catch (e: any) {
